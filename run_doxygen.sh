db40747eab3cc307802775ba0c60f7183104d8de
SuryaGrandhi <suryagrandhi97@gmail.com>
2019-03-25 15:31:51 +0530

    rocBLAS api5

--- run_doxygen.sh
+++ run_doxygen.sh
@@ -2,10 +2,10 @@
 
 #doxygen Doxyfile
 
-sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas.h > ROCm_Libraries/rocBLAS/src/include/rocblas.h
-sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-functions.h > ROCm_Libraries/rocBLAS/src/include/rocblas-functions.h
-sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-types.h > ROCm_Libraries/rocBLAS/src/include/rocblas-types.h
-sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-auxiliary.h > ROCm_Libraries/rocBLAS/src/include/rocblas-auxiliary.h
+sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas.h > ROCm_Libraries/rocBLAS/src/rocblas.h
+sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-functions.h > ROCm_Libraries/rocBLAS/src/rocblas-functions.h
+sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-types.h > ROCm_Libraries/rocBLAS/src/rocblas-types.h
+sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-auxiliary.h > ROCm_Libraries/rocBLAS/src/rocblas-auxiliary.h
 
 doxygen ROCm_Libraries/rocBLAS/Doxyfile
 
