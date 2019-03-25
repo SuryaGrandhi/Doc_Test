6d96572a9e2bd8467d9fc3b9d1f01cca0e93e510
SuryaGrandhi <suryagrandhi97@gmail.com>
2019-03-25 15:19:17 +0530

    rocBLAS api4

--- run_doxygen.sh
+++ run_doxygen.sh
@@ -2,14 +2,12 @@
 
 #doxygen Doxyfile
 
-sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas.h > rocblas.h
-sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-functions.h > rocblas-functions.h
-sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-types.h > rocblas-types.h
-sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-auxiliary.h > rocblas-auxiliary.h
+sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas.h > ROCm_Libraries/rocBLAS/src/include/rocblas.h
+sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-functions.h > ROCm_Libraries/rocBLAS/src/include/rocblas-functions.h
+sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-types.h > ROCm_Libraries/rocBLAS/src/include/rocblas-types.h
+sed -e 's/ROCBLAS_EXPORT //g' ROCm_Libraries/rocBLAS/src/include/rocblas-auxiliary.h > ROCm_Libraries/rocBLAS/src/include/rocblas-auxiliary.h
 
 doxygen ROCm_Libraries/rocBLAS/Doxyfile
-
-rm *.h
 
 #doxygen ROCm_Libraries/rocALUTION/Doxyfile
 
