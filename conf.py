3f1becc1227bb41fbc72dabe2bd128bc09e967d7
SuryaGrandhi <suryagrandhi97@gmail.com>
2019-03-26 15:47:06 +0530

    sections

--- conf.py
+++ conf.py
@@ -139,7 +139,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {'collapse_navigation': True,
-		      'navigation_depth': -1}
+		      'navigation_depth': 9}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
