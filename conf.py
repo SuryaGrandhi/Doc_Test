629797941e0cb798e73e2dfed0df726375293483
SuryaGrandhi <suryagrandhi97@gmail.com>
2019-04-17 12:23:44 +0530

    xelatex addition

--- conf.py
+++ conf.py
@@ -217,6 +217,8 @@ htmlhelp_basename = 'ReadTheDocs-Breathedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
+latex_engine = 'xelatex'
+
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
